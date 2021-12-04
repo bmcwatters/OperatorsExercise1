ff69ed6b1d3070fab117e5bf8fa3a9f4b2e12ce4
bryan mcwatters <bmcwatters@comcast.net>
2021-12-04 15:16:42 -0500

    initial commit

---
+++ OperatorsExercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 17;
+int b = 4;
+int quotient = a / b;
+int remainder = a % b;
+
+if (a == 17 && b == 4)
+{
+    Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+}
+
+Console.WriteLine(a + b);
+Console.WriteLine(a * b);
+Console.WriteLine(b - a);
+Console.WriteLine(a / b);
